647bfd53bca035ddbf8f9c18076fac9f1127bf55
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-07-15 22:44:21 +0200

    run in main

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get_image(url: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
     # ngrok_tunnel = ngrok.connect(8000)
     # print('Public URL:', ngrok_tunnel.public_url)
     # nest_asyncio.apply()
